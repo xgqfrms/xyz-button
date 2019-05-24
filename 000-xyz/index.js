8392254fb8b103b5b3dfd79315b431f57b1d1ab2
xgqfrms <xgqfrms@outlook.com>
2019-05-24 17:56:44 +0800

    1.0.5

--- 000-xyz/index.js
+++ 000-xyz/index.js
@@ -14,11 +14,13 @@
  */
 
 import XyzButton from "./xyz-button.vue";
-
+// import CorpCard from "./corp-card.vue";
+// import * as PxtoRem from "./utils/px2rem";
+// PxtoRem();
 
 export default {
     install(Vue, options) {
         Vue.component("xyz-button", XyzButton);
-        // Vue.component("XyzButton", XyzButton);
+        // Vue.component("crop-card", CorpCard);
     }
 };
